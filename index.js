4ef57326267a1bab6c150514c052888ec3046402
Erix Gutierrez <erix.gutierrez@gmail.com>
2023-04-13 14:42:12 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const URL = process.env.FRONTEND || "http://localhost:3000";
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: URL,
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (receiverId) => {
+  return users.find((user) => user.userId === receiverId);
+};
+
+io.on("connection", (socket) => {
+  socket.on("addUserToSocketArray", (userId) => {
+    addUser(userId, socket.id);
+    socket.emit("usersSocketsArray", users);
+  });
+
+  socket.on("sendMessage", ({ userId, receiverId, message }) => {
+    const user = getUser(receiverId);
+
+    socket.to(user?.socketId).emit("newMessage", {
+      userId,
+      message,
+      notification: true,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    // socket.emit("usersSocketsArray", users);
+  });
+});
